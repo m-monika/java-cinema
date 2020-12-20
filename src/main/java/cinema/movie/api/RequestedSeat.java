61ad34bfe6a140837f48bb1bc65b9793bf5fc5c5
Monika <m852.monika@gmail.com>
2020-12-20 09:51:01 +0100

    Requested seat added

---
+++ src/main/java/cinema/movie/api/RequestedSeat.java
@@ -0,0 +1,37 @@
+package cinema.movie.api;
+
+public class RequestedSeat {
+    private final int sector;
+    private final int row;
+    private final int seatInRow;
+
+    public RequestedSeat(int sector, int row, int seatInRow) {
+        if (sector < 1) {
+            throw new IllegalArgumentException("Sector must be greater than 0");
+        }
+
+        if (row < 1) {
+            throw new IllegalArgumentException("Row must be greater than 0");
+        }
+
+        if (seatInRow < 1) {
+            throw new IllegalArgumentException("Seat in row must be greater than 0");
+        }
+
+        this.sector = sector;
+        this.row = row;
+        this.seatInRow = seatInRow;
+    }
+
+    public int getSector() {
+        return sector;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getSeatInRow() {
+        return seatInRow;
+    }
+}
